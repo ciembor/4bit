216cac391c3c52da76bdaa0808f6a2c0c369db74
ciembor <pub@ciemborowicz.pl>
2012-08-26 23:13:28 +0200

    Faster build in development (compressing with production option).

--- build.sh
+++ build.sh
@@ -1,23 +1,58 @@
 #!/bin/bash
 
+##############################################
+### Helpers
+
+status () {
+	if [ $? -gt 0 ]; then
+		echo -ne "[\e[1;31mERROR\e[m]\n"
+		exit 1
+	else
+		echo -ne "[\e[1;32mOK\e[m]\n"
+	fi
+}
+
+##############################################
+### Paths
+
 APP_PATH=`pwd`
+CLOSURE_LIB="../closure"
+COMPILER="/usr/share/java/closure-compiler/closure-compiler.jar"
+
+##############################################
+### Options
+
+closure_options="-o script"
+less_options=""
+
+if [ $# -ne 2 ]
+then
+	if [[ "$1" = "production" ]]
+	then
+		closure_options="-o compiled -c ${COMPILER}"
+		less_options="-yui-compress"
+	fi
+fi
 
 ##############################################
 ### Compile JavaScript with Google Closure 
 
-CLOSURE_LIB="../closure"
-COMPILER="/usr/share/java/closure-compiler/closure-compiler.jar"
-
-python2 ${CLOSURE_LIB}/bin/calcdeps.py -i ${APP_PATH}/lib/js/jquery-ui-1.8.23.custom.js -i ${APP_PATH}/lib/js/jquery.ui.colorPicker.js -i ${APP_PATH}/js/main.js -p ${CLOSURE_LIB} -o compiled -c ${COMPILER} > ${APP_PATH}/js/compiled.js
+echo "==== Compiling JavaScript ======================================================"
+python2 ${CLOSURE_LIB}/bin/calcdeps.py -i ${APP_PATH}/lib/js/jquery-ui-1.8.23.custom.js -i ${APP_PATH}/lib/js/jquery.ui.colorPicker.js -i ${APP_PATH}/js/main.js -p ${CLOSURE_LIB} ${closure_options} > ${APP_PATH}/js/compiled.js
+status
 
 ##############################################
 ### Compile LESS
 
-lessc -i ${APP_PATH}/less/main.less -o ${APP_PATH}/css/_compiled_main.css -yui-compress
+echo "==== Compiling LESS ============================================================"
+lessc -i ${APP_PATH}/less/main.less -o ${APP_PATH}/css/_compiled_main.css ${less_options}
+status
 
 ##############################################
 ### Merge CSS
 
+echo "==== Merging CSS ==============================================================="
 cat ${APP_PATH}/css/jquery-ui-1.8.23.custom.css > ${APP_PATH}/css/merged.css
 cat ${APP_PATH}/css/jquery.ui.colorPicker.css >> ${APP_PATH}/css/merged.css
 cat ${APP_PATH}/css/_compiled_main.css >> ${APP_PATH}/css/merged.css
+status
