218f645ac5d9a50d074194bc7e13a917c036dcc2
ciembor <pub@ciemborowicz.pl>
2012-08-26 23:28:47 +0200

    Usage.

--- build.sh
+++ build.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-##############################################
-### Helpers
+################################################################################
+#
+# Usage:
+# 
+# ./build.sh                - building in development (simple merging)
+# ./build.sh production     - building in production (with compressing)
+#
+################################################################################
 
-status () {
-	if [ $? -gt 0 ]; then
-		echo -ne "[\e[1;31mERROR\e[m]\n"
-		exit 1
-	else
-		echo -ne "[\e[1;32mOK\e[m]\n"
-	fi
-}
-
-##############################################
+################################################################################
 ### Paths
 
 APP_PATH=`pwd`
 CLOSURE_LIB="../closure"
 COMPILER="/usr/share/java/closure-compiler/closure-compiler.jar"
 
-##############################################
+################################################################################
 ### Options
 
 closure_options="-o script"
@@ -34,22 +31,28 @@ then
 	fi
 fi
 
-##############################################
-### Compile JavaScript with Google Closure 
+################################################################################
+### Helpers
+
+status () {
+	if [ $? -gt 0 ]; then
+		echo -ne "[\e[1;31mERROR\e[m]\n"
+		exit 1
+	else
+		echo -ne "[\e[1;32mOK\e[m]\n"
+	fi
+}
+
+################################################################################
+### Building
 
 echo "==== Compiling JavaScript ======================================================"
 python2 ${CLOSURE_LIB}/bin/calcdeps.py -i ${APP_PATH}/lib/js/jquery-ui-1.8.23.custom.js -i ${APP_PATH}/lib/js/jquery.ui.colorPicker.js -i ${APP_PATH}/js/main.js -p ${CLOSURE_LIB} ${closure_options} > ${APP_PATH}/js/compiled.js
 status
 
-##############################################
-### Compile LESS
-
 echo "==== Compiling LESS ============================================================"
 lessc -i ${APP_PATH}/less/main.less -o ${APP_PATH}/css/_compiled_main.css ${less_options}
 status
-
-##############################################
-### Merge CSS
 
 echo "==== Merging CSS ==============================================================="
 cat ${APP_PATH}/css/jquery-ui-1.8.23.custom.css > ${APP_PATH}/css/merged.css
