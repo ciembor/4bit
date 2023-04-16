faae126fec05befd8634a1c7dd6b0851927864b7
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2023-04-17 00:39:20 +0200

    Remove Facebook button.

--- js/main.js
+++ js/main.js
@@ -7,15 +7,6 @@
 
 (function() {
 
-	// facebook button
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-
 	// twitter button
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
