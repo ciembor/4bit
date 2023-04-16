8027f50c9de03cbfcc65e0ad18bd8163c7906424
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2023-04-17 00:31:08 +0200

    Remove Google Plus button, changes in footer.

--- js/main.js
+++ js/main.js
@@ -19,13 +19,6 @@
 	// twitter button
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
-	// google plus button
-	(function() {
-		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-		po.src = 'https://apis.google.com/js/plusone.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	})();
-
 })();
 
 // jquery ui vertial radio ////////////////////////////////////////////////////////////////////////
